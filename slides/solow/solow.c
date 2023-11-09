aeb2d18db08f17cb1670ac17ca6527469f255a35
John Stachurski <john.stachurski@gmail.com>
2023-11-10 06:33:46 +1100

    misc

--- slides/solow/solow.c
+++ slides/solow/solow.c
@@ -4,11 +4,12 @@
 int main() {
     double k = 0.2;
     double alpha = 0.4;
+    double s = 0.3;
     double delta = 0.1;
     int i;
     int n = 1000;
     for (i = 0; i < n; i++) {
-        k = pow(k, alpha) + (1 - delta) * k;
+        k = s * pow(k, alpha) + (1 - delta) * k;
     }
     printf("k = %f\n", k);
 }
